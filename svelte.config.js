ef80e0b67e2ddafff24f0a335fc1537fc99b05dd
Albin Groen <albin.groen@gmail.com>
2021-09-15 22:28:15 +0200

    :wrench: Set up SvelteKit node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,8 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+    adapter: node()
 	}
 };
 
