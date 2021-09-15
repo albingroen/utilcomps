0285e0e180cec27e7db5d517787598b0c6655f5d
Albin Groen <albin.groen@gmail.com>
2021-09-15 20:03:58 +0200

    :lipstick: Small style updates

--- src/lib/TextInput.svelte
+++ src/lib/TextInput.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
-import { tw } from "twind";
-import classNames from "./classNames";
-import Stack from "./Stack.svelte";
+	import { tw } from 'twind';
+	import classNames from './classNames';
+	import Stack from './Stack.svelte';
 
-export let label: string = undefined;
+	export let label: string = undefined;
+	export let handleChange: (e: Event & { currentTarget: EventTarget & HTMLInputElement }) => void;
 </script>
 
 <Stack justify="between" align="center">
-  {#if label}
-    <label
-      class="{tw('w-1/5 text-sm text-right text-scorpion-300')}"
-      for="{$$props.id}">{label}{$$props.required ? "*" : ""}:</label>
-  {/if}
+	{#if label}
+		<label class={tw('w-1/5 text-sm text-right text-scorpion-300')} for={$$props.id}
+			>{label}{$$props.required ? '*' : ''}:</label
+		>
+	{/if}
 
-  <input
-    {...$$props}
-    class="{tw(
-      classNames(
-        'bg-scorpion-700 border border-[#494949] rounded-md py-1 text-white px-2 placeholder-scorpion-500 text-sm focus:outline-none focus:border-scorpion-400',
-        Boolean(label) && 'w-4/5',
-        $$props.classNames
-      )
-    )}" />
+	<input
+		{...$$props}
+		on:input={handleChange}
+		class={tw(
+			classNames(
+				'bg-scorpion-700 border border-[#494949] rounded-md py-1 text-white px-2 placeholder-scorpion-500 text-sm focus:outline-none focus:border-scorpion-400',
+				Boolean(label) && 'w-4/5',
+				$$props.classNames
+			)
+		)}
+	/>
 </Stack>
