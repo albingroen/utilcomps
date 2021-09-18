c513d526c7699ef1095dfe3d2277db5a16534be6
Albin Groen <albin.groen@gmail.com>
2021-09-18 16:47:47 +0200

    :lipstick: Normal whitespace on all text

--- src/lib/Text.svelte
+++ src/lib/Text.svelte
@@ -5,12 +5,18 @@
 	type TextLevel = 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span';
 
 	export let level: TextLevel = 'p';
+
+	const textStyles = {
+		base: 'whitespace-normal'
+	};
 </script>
 
 {#if level === 'p'}
 	<p
 		{...$$props}
-		class={tw(classNames('text-sm leading-relaxed text-scorpion-300', $$props.class))}
+		class={tw(
+			classNames(textStyles.base, 'text-sm leading-relaxed text-scorpion-300', $$props.class)
+		)}
 	>
 		<slot />
 	</p>
@@ -19,27 +25,27 @@
 		<slot />
 	</span>
 {:else if level === 'h1'}
-	<h1 {...$$props} class={tw(classNames('text-xl text-white', $$props.class))}>
+	<h1 {...$$props} class={tw(classNames(textStyles.base, 'text-xl text-white', $$props.class))}>
 		<slot />
 	</h1>
 {:else if level === 'h2'}
-	<h2 {...$$props} class={tw(classNames('text-lg text-white'), $$props.class)}>
+	<h2 {...$$props} class={tw(classNames(textStyles.base, 'text-lg text-white'), $$props.class)}>
 		<slot />
 	</h2>
 {:else if level === 'h3'}
-	<h3 {...$$props} class={tw(classNames('text-base text-white', $$props.class))}>
+	<h3 {...$$props} class={tw(classNames(textStyles.base, 'text-base text-white', $$props.class))}>
 		<slot />
 	</h3>
 {:else if level === 'h4'}
-	<h4 {...$$props} class={tw(classNames('text-sm text-white', $$props.class))}>
+	<h4 {...$$props} class={tw(classNames(textStyles.base, 'text-sm text-white', $$props.class))}>
 		<slot />
 	</h4>
 {:else if level === 'h5'}
-	<h5 {...$$props} class={tw(classNames('text-xs text-white', $$props.class))}>
+	<h5 {...$$props} class={tw(classNames(textStyles.base, 'text-xs text-white', $$props.class))}>
 		<slot />
 	</h5>
 {:else if level === 'h6'}
-	<h6 {...$$props} class={tw(classNames('text-2xs text-white', $$props.class))}>
+	<h6 {...$$props} class={tw(classNames(textStyles.base, 'text-2xs text-white', $$props.class))}>
 		<slot />
 	</h6>
 {/if}
